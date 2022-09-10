0f665b42244d1d1e2e8ce81cbb037ed20c3fe337
bbster <bbster12@gmail.com>
2022-09-10 21:22:48 +0900

    상속, super클래스

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
-class Human:
-    def __init__(self, name):
-        print("huma initialized")
-        self.name = name
+# class Human:
+#     def __init__(self, name):
+#         print("huma initialized")
+#         self.name = name
+#
+#     def say_hello(self):
+#         print(f"hello my name is {self.name}")
+#
+#
+# class Player(Human):
+#     def __init__(self, name, xp): # self는 class의 인스턴스
+#         super().__init__(name)
+#         self.name = name
+#
+#
+# class Fan(Human):
+#     def __init__(self, name, fav_team):
+#         super().__init__(name)
+#         self.fav_team = fav_team
+#
+# nico = Player("nico", 10)
+# nico.say_hello()
+#
+class Dog:
+    def woof(self):
+        print("woofwoof")
 
-    def say_hello(self):
-        print(f"hello my name is {self.name}")
 
+class Beagle(Dog):
+    def woof(self):
+        super().woof()
+        print("jump")
 
-class Player(Human):
-    def __init__(self, name, xp): # self는 class의 인스턴스
-        self.name = name
-
-
-class Fan(Human):
-    def __init__(self, name, fav_team):
-        super().__init__(name)
-        self.fav_team = fav_team
-
-nico = Fan("nico", "blue")
+beagle = Beagle()
+beagle.woof()
