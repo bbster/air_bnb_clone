c3d65665f7a52b28428a32c30411186a1c8155ae
bbster <bbster12@gmail.com>
2022-09-11 17:18:00 +0900

    change db -> sqlite3, create superuser, create houses app

--- scripts/connect_docker_postgresql.sh
+++ scripts/connect_docker_postgresql.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-container_name=PostgreSQL
-docker exec -it $container_name /bin/bash
+--- docker images
+--- docker 이미지로 컨테이너 생성
+docker run -d -p 5432:5432 -e POSTGRES_PASSWORD="1r2r3r4r" --name PostgreSQL postgres
+
+docker exec -it PostgreSQL /bin/bash
--- scripts/create_database.sh
+++ scripts/create_database.sh
@@ -8,10 +8,10 @@ psql -U postgres - postgres 접속
 CREATE DATABASE air_bnb_db;
 
 --- User 생성
-CREATE USER admin WITH PASSWORD '1r2r3r4r';
-ALTER ROLE admin SET client_encoding TO 'utf8';
-ALTER ROLE admin SET default_transaction_isolation TO 'read committed';
-ALTER ROLE admin WITH SUPERUSER;
+CREATE USER bnb WITH PASSWORD '1r2r3r4r';
+ALTER ROLE bnb SET client_encoding TO 'utf8';
+ALTER ROLE bnb SET default_transaction_isolation TO 'read committed';
+ALTER ROLE bnb WITH SUPERUSER;
 
 --- DB에 대한 User 권한 부여
-GRANT ALL PRIVILEGES ON DATABASE air_bnb_db TO admin;
+GRANT ALL PRIVILEGES ON DATABASE air_bnb_db TO bnb;
